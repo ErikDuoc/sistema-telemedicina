0ce960d42670d931b89b4e875361599d0e39ceff
GenaLagos10 <gena.lagos@duocuc.cl>
2026-06-21 13:03:37 -0400

    feat: add swagger configuration to clinical-record and laboratory services

---
+++ clinical-record-service/src/main/java/cl/duoc/fullstack/clinicalrecordservice/config/OpenApiConfig.java
@@ -0,0 +1,30 @@
+package cl.duoc.fullstack.clinicalrecordservice.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI clinicalrecordserviceOpenApi() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Clinical Records API")
+                        .description("API REST para gestión de fichas clínicas")
+                        .version("1.0.0")
+                        .license(new License().name("Uso personal")))
+                .addSecurityItem(new SecurityRequirement().addList("basicAuth"))
+                .components(new Components()
+                        .addSecuritySchemes("basicAuth", new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("basic")));
+    }
+}
+
