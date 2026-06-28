fa513f5369a272aa9d0717f640d1ad76cf135650
Erik Queirolo <er.queirolo@duocuc.cl>
2026-06-28 14:15:47 -0400

    [feat] patchMapping faltante en agendaClient

--- appointment-service/src/main/java/cl/duoc/fullstack/appointmentservice/client/AgendaClient.java
+++ appointment-service/src/main/java/cl/duoc/fullstack/appointmentservice/client/AgendaClient.java
@@ -8,7 +8,4 @@ public interface AgendaClient {
 
     @GetMapping("/api/agenda/doctor/{id}")
     Object getDoctorAgenda(@PathVariable Long id);
-
-    @PatchMapping("/api/agenda/blocks/{id}/reserve")
-    void reserveBlock(@PathVariable Long id);
 }
